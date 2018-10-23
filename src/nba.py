b55940af22eb31abc4363c741171fe9f3aa53fcd
weilbach <weilbach@umich.edu>
2018-10-23 15:22:17 -0400

    first commit

---
+++ src/nba.py
@@ -0,0 +1,15 @@
+import requests
+
+url = 'http://stats.nba.com/stats/commonteamroster?LeagueID=00&Season=2017-18&TeamID=1610612756'
+request_headers = {
+    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Host': 'stats.nba.com',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+    }
+response=requests.get(url, headers = request_headers)
+print (response.text)
